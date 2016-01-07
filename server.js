51a205636c8c4e371763e3b2c12019b100a0b24e
Marina Delerce-Mauris <marina.delerce@gmail.com>
2016-01-07 11:50:38 +0100

    routes.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var fs = require("fs");
 // We need to use the express framework: have a real web server that knows how to send mime types etc.
 var express=require('express');
 
+var routes = require('server/routes');
+
 // Init globals variables for each module required
 var app = express()
 , http = require('http')
@@ -19,71 +21,5 @@ var PORT = 8081,
 // launch the http server on given port
 server.listen(PORT);
 
-// routing
-app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
-});
-
-// routing
-app.get('/track', function (req, res) {
-	function sendTracks(trackList) {
-		if (!trackList)
-			return res.send(404, 'No track found');
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.write(JSON.stringify(trackList));
-		res.end();
-	}
-
-	getTracks(sendTracks); 
-});
-
-// routing
-app.get('/track/:id', function (req, res) {
-	var id = req.params.id;
-	
-	function sendTrack(track) {
-		if (!track)
-			return res.send(404, 'Track not found with id "' + id + '"');
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.write(JSON.stringify(track));
-		res.end();
-	}
-
-	getTrack(id, sendTrack); 
-
-});
-
-// routing
-app.get(/\/track\/(\w+)\/(?:sound|visualisation)\/((\w|.)+)/, function (req, res) {
-	res.sendfile(__dirname + '/' + TRACKS_PATH + req.params[0] + '/' + req.params[1]);
-});
-
-function getTracks(callback) {
-	getFiles(TRACKS_PATH, callback);
-}
-
-function getTrack(id, callback) {
-	getFiles(TRACKS_PATH + id, function(fileNames) {
-		var track = {
-			id: id,
-			instruments: []	
-		};
-		fileNames.sort();
-		for (var i = 0; i < fileNames.length; i += 2) {
-			var instrument = fileNames[i].match(/(.*)\.[^.]+$/, '')[1];
-			track.instruments.push({
-				name: instrument,
-				sound: instrument + '.mp3',
-				visualisation: instrument + '.png'
-			});
-		}
-		callback(track);
-	})
-}
-
-function getFiles(dirName, callback) {
-	fs.readdir(dirName, function(error, directoryObject) {
-		callback(directoryObject);
-	});
-}
+app.use('/', routes);
 
