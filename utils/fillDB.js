634d300b77a2069527c90cf08d2b62cab4b19dba
Erezann <lorderezan@gmail.com>
2016-01-21 10:20:13 +0100

    changement de dossier de fillDB.js et de data.json

---
+++ utils/fillDB.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient;
+var fs = require("fs");
+var assert = require("assert");
+var song = require('./songDB');
+
+// putting the data into the 'test' database:
+MongoClient.connect('mongodb://127.0.0.1:27017/prod', function(err, db) {
+    if(err) throw err;
+    db.collection(song.getSongDB()).deleteMany({}, function(err, results) {
+        var data = fs.readFileSync("data.json");
+        var JSONData = JSON.parse(data);
+        db.collection(song.getSongDB()).insertMany(JSONData, function(err, result) {
+            assert.equal(null, err);
+            console.log("Number of inserted documents : "+result.insertedCount);
+            db.close();
+        });
+    });
+});
