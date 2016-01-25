e77438e907dc1494970141763289af836a57216b
Riana Rabehasy <rabehasy@polytech.unice.fr>
2016-01-25 16:37:07 +0100

    MongoClient enlevé

--- server/mixDB.js
+++ server/mixDB.js
@@ -1,7 +1,4 @@
-var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/prod';
 var fs = require('fs');
 var path = require('path');
 
