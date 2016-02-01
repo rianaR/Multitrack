41f2d61ee5f12304c4d13e8c23da2432d67a2fd8
Erezann <lorderezan@gmail.com>
2016-02-01 15:08:49 +0100

    ajout de test dans userDBtest

--- test/userDBTest.js
+++ test/userDBTest.js
@@ -8,21 +8,50 @@ describe('userDB test', function() {
 
     //remove all the mix and all users each time
     beforeEach(function(done){
-	mix.removeAllMix(function(){
+	mix.removeAllMixes(function(){
 	    user.removeAllUsers(function(){
 		done();
 	    });
 	});
     });
 
-    it('should not have mixis and users',function(done){
-	mix.getAllMix(function(err,results){
+    it('should not have mixes and users',function(done){
+	mix.getAllMixes(function(err,results){
+	    assert.equal(err,null);
 	    assert.deepEqual(results,[]);
 	    user.getUsers(function(err,results){
+		assert.equal(err,null);
 		assert.deepEqual(results,[]);
 		done();
 	    });
 	});
     });
+
+    it('should add two users then return them',function(done){
+	user.addUser("user1","normal",function(err, results){
+	    assert.equal(err,null);
+	    user.addUser("user2","normal",function(err, results){
+		assert.equal(err,null);
+		user.getUsers(function(err, results){
+		    assert.equal(err,null);
+		    if(results[0].name=="user1"){
+			assert.equal(results[0].name,"user1");
+			assert.equal(results[0].right,"normal");
+			assert.equal(results[1].name,"user2");
+			assert.equal(results[1].right,"normal");
+		    }
+		    else{
+			assert.equal(results[0].name,"user2");
+			assert.equal(results[0].right,"normal");
+			assert.equal(results[1].name,"user1");
+			assert.equal(results[1].right,"normal");
+		    }
+		    done();
+		});
+	    });
+	});
+    });
+
+    
 
 });
